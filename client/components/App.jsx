fa1308f90cc4c4ebc4cf29ef66723a903e98af9d
xxjrtxx <xxjrtxx@pop-os.localdomain>
2024-02-18 15:37:46 -0600

    Setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import Header from './Header.jsx';
 import Main from './Main.jsx';
-import Carousel from './Carousel.jsx';
 import Footer from './Footer.jsx';
 
 const App = () => {
